3e3403979c616085b2e52ed1d49e3394e09e0572
Gaiercop <gaiercop@gmail.com>
2025-01-24 11:58:59 +0300

    Added index.html

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ def catalogs():
     catalogs = ['Electronics', 'Books', 'Clothing', 'Home & Garden']
     return render_template('catalogs.html', catalogs=catalogs)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
